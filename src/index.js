0f53bdfeffb4938179556fea695f2787d89b95a9
Sam Davis <hsndm16@yahoo.com>
2021-12-20 10:26:06 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-
-import './index.css';
-
 import App from './App';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import BaseLayout from './components/layout/BaseLayout'
